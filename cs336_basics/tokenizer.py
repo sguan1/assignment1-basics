2e57e13dcd8cf5380e7ff41939d152b7bd53c0d9
Shen Guan <sguan1@alumni.cmu.edu>
2025-08-07 04:57:57 +0000

    Add implementation for tokenizer

---
+++ cs336_basics/tokenizer.py
@@ -0,0 +1,159 @@
+from collections.abc import Iterable, Iterator
+import regex as re
+import os
+import json
+from cs336_basics.utils.io import gpt2_bytes_to_unicode
+
+def get_tokenizer_from_vocab_merges_path(
+    vocab_path: str | os.PathLike,
+    merges_path: str | os.PathLike,
+    special_tokens: list[str] | None = None,
+):
+    gpt2_byte_decoder = {v: k for k, v in gpt2_bytes_to_unicode().items()}
+    with open(vocab_path) as vocab_f:
+        gpt2_vocab = json.load(vocab_f)
+    gpt2_bpe_merges = []
+    with open(merges_path) as f:
+        for line in f:
+            cleaned_line = line.rstrip()
+            if cleaned_line and len(cleaned_line.split(" ")) == 2:
+                gpt2_bpe_merges.append(tuple(cleaned_line.split(" ")))
+    # The GPT-2 tokenizer uses a remapped unicode encoding for bytes. Let's
+    # just return the original bytes, so we don't force students to use
+    # any particular encoding scheme.
+    vocab = {
+        gpt2_vocab_index: bytes([gpt2_byte_decoder[token] for token in gpt2_vocab_item])
+        for gpt2_vocab_item, gpt2_vocab_index in gpt2_vocab.items()
+    }
+    # If any of the special tokens don't exist in the vocab, append them to the vocab.
+    if special_tokens:
+        for special_token in special_tokens:
+            byte_encoded_special_token = special_token.encode("utf-8")
+            if byte_encoded_special_token not in set(vocab.values()):
+                vocab[len(vocab)] = byte_encoded_special_token
+
+    merges = [
+        (
+            bytes([gpt2_byte_decoder[token] for token in merge_token_1]),
+            bytes([gpt2_byte_decoder[token] for token in merge_token_2]),
+        )
+        for merge_token_1, merge_token_2 in gpt2_bpe_merges
+    ]
+    return vocab, merges
+
+PAT = re.compile(r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+""")
+
+class Tokenizer:
+
+    def __init__(self, vocab: dict[int, bytes], merges: list[tuple[bytes, bytes]], special_tokens:list[str] | None = None):
+        """Construct a tokenizer from a given
+        vocabulary, list of merges, and (optionally) a list of special tokens. This function should accept the following parameters:
+        vocab: dict[int, bytes]
+        merges: list[tuple[bytes, bytes]]
+        special_tokens: list[str] | None = None"""
+        self.vocab = vocab
+        self.vocab_inv ={value: key for key,value in vocab.items()}
+        self.merges = merges
+        self.merges_order = {(self.vocab_inv[merge[0]], self.vocab_inv[merge[1]]): self.vocab_inv[merge[0] + merge[1]] for merge in merges}
+        self.special_tokens = None
+        if special_tokens:
+            self.special_tokens = sorted(special_tokens, key = len, reverse=True)
+            next_id = max(self.vocab.keys()) + 1
+            for special_token in self.special_tokens:
+                encoded_token = special_token.encode("UTF_8")
+                if encoded_token not in self.vocab_inv:
+                    self.vocab_inv[encoded_token] = next_id
+                    self.vocab[next_id] = encoded_token
+                    next_id += 1
+
+    @classmethod
+    def from_files(cls, vocab_filepath: str, merges_filepath:str, special_tokens:list[str] | None = None):
+        """Class
+        method that constructs and return a Tokenizer from a serialized vocabulary and list of merges
+        (in the same format that your BPE training code output) and (optionally) a list of special
+        tokens. This method should accept the following additional parameters:
+        vocab_filepath: str
+        merges_filepath: str
+        special_tokens: list[str] | None = None"""
+        vocab, merges = get_tokenizer_from_vocab_merges_path(vocab_filepath, merges_filepath, special_tokens)
+        return cls(vocab, merges, special_tokens)
+        
+
+    def encode(self, text: str) -> list[int]:
+        """Encode an input text into a sequence of token IDs.
+        def encode_iterable(self, iterable: Iterable[str]) -> Iterator[int] Given an iterable of
+        strings (e.g., a Python file handle), return a generator that lazily yields token IDs. This is
+        required for memory-efficient tokenization of large files that we cannot directly load into
+        memory."""
+
+        if not self.special_tokens:
+            return self.merge(text)
+        
+        result = []
+        # Split on special toekns and keep delimiters
+        pattern = "(" + "|".join(re.escape(token) for token in self.special_tokens) + ")"
+        subtexts = re.split(pattern, text)
+        for subtext in subtexts:
+            if subtext in self.special_tokens:
+                result.append(self.vocab_inv[subtext.encode("UTF-8")])
+            else:
+                result.extend(self.merge(subtext))
+        return result
+    
+    def encode_iterable(self, iterable: Iterable[str]) -> Iterator[int]:
+        """Given an iterable of
+        strings (e.g., a Python file handle), return a generator that lazily yields token IDs. This is
+        required for memory-efficient tokenization of large files that we cannot directly load into
+        memory."""
+        for text in iterable:
+            yield from self.encode(text)
+                
+    def decode(self, ids: list[int]) -> str:
+        """Decode a sequence of token IDs into text"""
+        text = b''.join(self.vocab[id] for id in ids)
+        return text.decode("utf-8", errors="replace")
+
+    def merge(self, text:str) -> list[int]:
+        result=[]
+        pairs=[]
+        for match in PAT.finditer(text):
+            group = match.group()
+            ids = [self.vocab_inv[bytes([b])] for b in group.encode("utf-8")]
+            while len(ids) >= 2:
+                pairs = self.getPairs(ids)
+                pair = min(pairs, key = lambda e: self.merges_order.get(e, float('inf')))
+                if pair not in self.merges_order:
+                    break
+                id = self.merges_order[pair]
+                ids = self.update(ids, id, pair)
+            result.extend(ids)
+        return result
+
+    def getPairs(self, ids: list[int]) -> list[int]:
+        pairs = []
+        for i in range(len(ids) - 1):
+                pair = (ids[i], ids[i + 1])
+                pairs.append(pair)
+        return pairs
+
+    def update(self, ids: list[int], id: int, pair: tuple[int, int])-> list[int]:
+        result = []
+        i = 0
+        while i < len(ids):
+            if i < len(ids) - 1 and ids[i] == pair[0] and ids[i + 1] == pair[1]:
+                result.append(self.vocab_inv[self.vocab[pair[0]] + self.vocab[pair[1]]])
+                i += 2
+            else:
+                result.append(ids[i])
+                i += 1
+        return result    
+    
+
+tokenizer = Tokenizer.from_files("./tests/fixtures/gpt2_vocab.json", "./tests/fixtures/gpt2_merges.txt", ["<|endoftext|>"])
+encode_string = tokenizer.encode("Héllò hôw <|endoftext|><|endoftext|> are ü? 🙃<|endoftext|>")
+
+
+                
+
+
+
--- tests/adapters.py
+++ tests/adapters.py
@@ -4,6 +4,7 @@ import os
 from typing import IO, Any, BinaryIO
 from collections.abc import Iterable
 from cs336_basics.train_bpe import train_bpe
+from cs336_basics.tokenizer import Tokenizer
 from jaxtyping import Float, Int
 
 import numpy.typing as npt
@@ -557,7 +558,7 @@ def get_tokenizer(
     Returns:
         A BPE tokenizer that uses the provided vocab, merges, and special tokens.
     """
-    raise NotImplementedError
+    return Tokenizer(vocab, merges, special_tokens)
 
 
 def run_train_bpe(
